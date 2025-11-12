7d422b487768554d002639f1ed58291e159093fc
orr <orr@MAENOTEJVE03051>
2025-11-12 18:17:07 -0300

    Adicionar arquivos de projeto.

---
+++ DirectoryFunctions/Pasta.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiretorioCRUD
+{
+    public class Pasta
+    {
+        // Caminhos BASE eles ficam armazenado para direcionar onde será criado a pasta etc...
+        private static List<string> caminhosBase = new List<string>()
+        {
+            @"\\JUPIAFS\Departamento de Informática\TesteOtavio",
+            @"C:\Users\orr\Desktop"
+        };
+        public static string caminhoCompleto { get; set; }
+
+        //CODIGOOOOOOO para juntar caminho com o nome da pasta       caminhoCompleto = Path.Combine(CaminhoBase, nomePasta);
+
+
+        public static void CriarPasta()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Escolha em qual local você irá criar a pasta");
+            Console.WriteLine();
+
+            for (int i = 0; i <  caminhosBase.Count; i++)
+            {
+                Console.WriteLine((i + 1) +" - "+caminhosBase[i]);
+            }
+
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            if (!int.TryParse(input, out int escolha) || escolha < 1 || escolha > caminhosBase.Count)
+            {
+                Console.WriteLine("Digite um valor valido (" + 1 + "-" + caminhosBase.Count + ") por favor");
+                Console.Write("Aperte enter...");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+            }
+
+
+            string caminhoEscolhido = caminhosBase[escolha];
+
+
+            Console.WriteLine("Digite o nome da pasta ou (v/voltar) para voltar: ");
+            string resposta = Console.ReadLine();
+            string nomePasta;
+
+            if (string.Equals(resposta, "voltar", StringComparison.OrdinalIgnoreCase)
+                || string.Equals(resposta, "v", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+            else
+            {
+                nomePasta = resposta;
+            }
+
+            caminhoCompleto = Path.Combine(caminhoEscolhido, nomePasta);
+
+            if (Directory.Exists(caminhoCompleto)) // verifica se ja existe diretorio com aquele nome
+            {
+                Console.Clear();
+                Console.WriteLine("Não foi possivel criar a pasta, pois este nome ja existe!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else // caso não exista a pasta, o codigo cria e imprimi na tela uma mensagem confirmando a operação
+            {
+                Console.Clear();
+                Directory.CreateDirectory(caminhoCompleto);
+                if (Directory.Exists(caminhoCompleto))
+                {
+                    Console.WriteLine("Pasta criada com sucesso!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possivel criar esta pasta!");
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ DirectoryFunctions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace DiretorioCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool rodando = true;
+            Pasta pasta = new Pasta();
+
+
+            while (rodando == true)
+            {
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("(1) - Criar Pastas");
+                Console.WriteLine("(2) - Editar Nome da Pastas");
+                Console.WriteLine("(3) - Listar Pastas");
+                Console.WriteLine("(4) - Excluir Pastas");
+                Console.WriteLine("(0) - Sair");
+                Console.WriteLine();
+
+                string input = (Console.ReadLine()); // variavel - escolha da ação
+
+                if(!int.TryParse(input, out int escolha))
+                {
+                    Console.WriteLine("Digite um valor valido 0-4 por favor");
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (escolha - 1)
+                {
+                    case 0:
+                        rodando = false;
+                        break;
+
+                        case 1:
+                        Pasta.CriarPasta();
+                        break;
+                }
+            }
+
+
+
+            
+        }
+    }
+}
